ae9237b0d79f18ef5cbf43d32ec88d423320e0ff
Alex Cleveland <Alexcleveland5@gmail.com>
2022-10-25 16:49:36 -0600

    Start of thought model

---
+++ models/Thought.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+
+        }
+    }
+)
