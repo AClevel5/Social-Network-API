e9d0b097d931947d6a2dd20598b4bea1252065f3
Alex Cleveland <Alexcleveland5@gmail.com>
2022-10-26 16:28:22 -0600

    Sudo code routes/controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+
+
+
+
+//Get to get all thoughts
+
+//Get to get a single thought by its _id
+
+//Post to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+//Put to update a thought by its _id
+
+//Delete to remove a thought by its _id
+
+//api/thoughts/:thoughtId/reactions POST to create a reaction stored in a single thought's reactions array field
+
+///api/thoughts/:thoughtId/reactions/:reactionId DELETE to pull and remove a reaction by the reaction's reactionId value
---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+
+
+
+
+// /api/users Get all users
+
+// /api/users Get a single user by its _id
+
+// /api/users Post a new user
+
+// /api/users Put to update user by its _id
+
+// /api/users Delete to remove a user by its _id
+
+
